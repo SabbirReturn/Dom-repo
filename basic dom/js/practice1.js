153722bd6dfe73635ac970d8c21679fedb48f12f
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-18 20:52:07 +0600

    create a html

--- basic dom/js/practice1.js
+++ basic dom/js/practice1.js
@@ -1,2 +1,51 @@
-let liList = document.getElementsByTagName('li');
-console.log(liList)
+// let liList = document.getElementsByTagName('li');
+// console.log(liList);
+
+// let h1Collections = document.getElementsByClassName('section-title');
+// for(let h1Collection of h1Collections){
+//     h1Collection.innerText
+//     // console.log(h1Collection)
+// }
+
+// let styles = document.getElementById('section-title');
+// styles.style.backgroundColor = ' Blue'
+// styles.style.textAlign = 'center'
+// styles.style.color = 'White'
+// styles.style.fontSize = '20px'
+
+
+// let liadd = document.getElementById('second-li');
+
+// let li = document.createElement('li');
+// li.innerText = 'List item-5'
+// liadd.appendChild(li)
+
+// let main = document.getElementById('main-container')
+// let section = document.createElement('section');
+// section.innerHTML =`
+//     <h1> My name is Sabbir Hasan </h1>
+//     <p>hi </p>
+// `
+// main.appendChild(section)
+
+const main = document.getElementById('main-container');
+const section = document.createElement('section');
+// section.innerHTML = `
+//     <h1>My Name is Sabbir Hasan</h1>
+//     <ul>
+//         <li>List item-1</li>
+//         <li>List item-2</li>
+//         <li>List item-3</li>
+//         <li>List item-4</li>
+//     </ul>
+// `
+// main.appendChild(section);
+
+
+
+
+
+ 
+
+
+
