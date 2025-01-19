84de340316e99778ed6ee1bcd4cafb9b2e75e4c9
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-20 03:07:21 +0600

    learning onclick

--- js-dom-events/js/index.js
+++ js-dom-events/js/index.js
@@ -1,5 +1,22 @@
-console.log('dom')
-
+// option1
+// direct use onclick in button
 let line = document.getElementById('title');
 line.setAttribute('title','hey I am sabbir Hasan')
 
+// option2
+function makeYellow(){
+    document.body.style.backgroundColor='Yellow';
+}
+
+// option3
+let makePurple = document.getElementById('make-purple');
+makePurple.onclick = makePurpleBtn;
+
+function makePurpleBtn(){
+    document.body.style.backgroundColor='purple';
+}
+// option4
+let makeBlueBtn = document.getElementById('make-blue');
+makeBlueBtn.onclick = function blueBtn(){
+    document.body.style.backgroundColor='blue';
+}
