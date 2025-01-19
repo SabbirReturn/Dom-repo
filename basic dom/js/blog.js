cf90e6c085bd387ef9051db2f0ebc47245773d78
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-19 23:04:42 +0600

    creat a section

--- basic dom/js/blog.js
+++ basic dom/js/blog.js
@@ -1,1 +1,31 @@
-console.log("hello");
+// console.log("hello");
+
+// let main = document.getElementById(main-container);
+
+// let section = document.createElement('section');
+
+// section.innerHTML =`
+//     <p>Hey</P>
+// `
+// main.appendChild(section);
+
+
+let main = document.getElementById('main-container');
+let section = document.createElement('section');
+section.innerHTML = `
+    <p>hey</p>
+`
+main.appendChild(section);
+
+// const main = document.getElementById('main-container');
+// const section = document.createElement('section');
+// section.innerHTML = `
+//     <h1>My Name is Sabbir Hasan</h1>
+//     <ul>
+//         <li>List item-1</li>
+//         <li>List item-2</li>
+//         <li>List item-3</li>
+//         <li>List item-4</li>
+//     </ul>
+// `
+// main.appendChild(section);
