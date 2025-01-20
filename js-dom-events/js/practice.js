64c60f57422429fb25f67fe4af486f252112f37e
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-20 21:01:08 +0600

    know some function about btn

--- js-dom-events/js/practice.js
+++ js-dom-events/js/practice.js
@@ -12,3 +12,18 @@ let makePurple = document.getElementById('make-purple');
 makePurple.onclick = function purple(){
     document.body.style.backgroundColor='purple'
 }
+
+let makeGreenBtn = document.getElementById('make-green');
+makeGreenBtn.addEventListener('click', makeGreen);
+
+function makeGreen(){
+    document.body.style.backgroundColor='Green'
+}
+let makePinkBtn = document.getElementById('make-pink');
+makePinkBtn.addEventListener('click', function(){
+    document.body.style.backgroundColor='Pink'
+})
+
+document.getElementById('make-golden').addEventListener('click', function(){
+    document.body.style.backgroundColor = 'Goldenrod'
+})
