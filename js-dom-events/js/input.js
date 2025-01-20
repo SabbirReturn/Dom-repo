7bb8a9695ff9e0cb7d1edb96c3fac2a47bab8698
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-21 02:12:47 +0600

    change title

--- js-dom-events/js/input.js
+++ js-dom-events/js/input.js
@@ -1,8 +1,19 @@
-document.getElementById('update-btn').addEventListener('click',function(){
-    let textField = document.getElementById('input-text')
-    let inputText = textField.value
+// document.getElementById('update-btn').addEventListener('click',function(){
+//     let textField = document.getElementById('input-text')
+//     let inputText = textField.value
 
-    let text = document.getElementById('text-display');
-    text.innerText = inputText;
-    textField.value ='';
+//     let text = document.getElementById('text-display');
+//     text.innerText = inputText;
+//     textField.value ='';
+// })
+
+document.getElementById('update-btn').addEventListener('click',function(){
+    // let textField = document.getElementById('input-text');
+    // let text = textField.value
+    // let changeLine = document.getElementById('input-direction')
+    // changeLine.innerText = text;
+    // textField.value = '';
+
+    let textLine = document.getElementById('input-direction');
+    textLine.innerText = 'Drop Your password'
 })
