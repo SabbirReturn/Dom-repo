7cf24cfcf7e255c87199b822ec48891b5d6ab1c7
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-25 03:28:04 +0600

    using tagName and get innerText

---
+++ Recap-dom/js/index.js
@@ -0,0 +1,9 @@
+let allLi = document.getElementsByTagName('li')
+for(let li of allLi){
+    console.log(li.innerText);
+}
+
+let allHeadings = document.getElementsByTagName('h1');
+for( let heading of allHeadings){
+    console.log(heading.innerText);
+}
