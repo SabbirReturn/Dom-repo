52a1dad0c0a7f9a52f338348548258df2184eeab
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-26 02:02:31 +0600

    index

--- Recap-dom/js/index.js
+++ Recap-dom/js/index.js
@@ -1,9 +1,17 @@
 let allLi = document.getElementsByTagName('li')
 for(let li of allLi){
-    console.log(li.innerText);
+    // console.log(li.innerText);
 }
 
 let allHeadings = document.getElementsByTagName('h1');
 for( let heading of allHeadings){
-    console.log(heading.innerText);
+    // console.log(heading.innerText);
 }
+
+let fruitsName = document.getElementsByClassName('fruits-name');
+for(let fruit of fruitsName){
+    // console.log(fruit.innerText);
+}
+
+let fruitTitle = document.getElementById('fruit-title');
+fruitTitle.innerText = 'Change the Title by Js'
