fbf0ff804bbf64dbe6066def7bd76bc2b2982209
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-26 23:18:46 +0600

    attribute

--- Recap-dom/js/index.js
+++ Recap-dom/js/index.js
@@ -13,11 +13,33 @@ for(let fruit of fruitsName){
     // console.log(fruit.innerText);
 }
 
-let fruitTitle = document.getElementById('fruit-title');
-fruitTitle.innerText = 'Change the Title by Js'
+// let fruitTitle = document.getElementById('fruit-title');
+// fruitTitle.innerText = 'Change the Title by Js'
 
 let someLi = document.querySelectorAll('#fruits-container li');
 for(let li of someLi){
-    console.log(li.innerText);
-    
+    // console.log(li.innerText);
+
 }
+
+let title = document.getElementById('fruits-container');
+title.getAttribute('id');
+title.getAttribute('class');
+title.classList.remove('blue-bg');
+title.classList.add('blue');
+console.log(title);
+
+
+let fruitTitle = document.getElementById('fruit-title');
+fruitTitle.setAttribute('title','I like this type od Food');
+console.log(fruitTitle);
+fruitTitle.style.backgroundColor = 'yellow'
+
+
+
+
+
+
+
+
+
