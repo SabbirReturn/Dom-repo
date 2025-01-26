dc33368ebbafb51b19a9234bcc2966b8960e5100
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-26 23:28:12 +0600

    section

--- Recap-dom/js/index.js
+++ Recap-dom/js/index.js
@@ -30,10 +30,22 @@ title.classList.add('blue');
 console.log(title);
 
 
-let fruitTitle = document.getElementById('fruit-title');
-fruitTitle.setAttribute('title','I like this type od Food');
-console.log(fruitTitle);
-fruitTitle.style.backgroundColor = 'yellow'
+// let fruitTitle = document.getElementById('fruit-title');
+// fruitTitle.setAttribute('title','I like this type od Food');
+// console.log(fruitTitle);
+// fruitTitle.style.backgroundColor = 'yellow'
+
+let sections = document.querySelectorAll('section');
+for(let section of sections){
+    section.style.backgroundColor='yellow'
+    section.style.marginBottom = '10px'
+    section.style.padding = '10px';
+    section.style.border = '2px solid red'
+}
+let fruitsColor = document.getElementById('fruits-container');
+fruitsColor.classList.add('fruits')
+
+
 
 
 
