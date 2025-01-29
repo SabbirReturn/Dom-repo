ba40cf6973d4a8a796bcb05df63c323e85a98759
SabbirReturn <mdsabbirhasan39@gmail.com>
2025-01-30 03:02:57 +0600

    btn

--- Recap-dom/js/github.js
+++ Recap-dom/js/github.js
@@ -1,4 +1,18 @@
 document.getElementById('delete-btn').addEventListener('click',function(){
     let infoField = document.getElementById('info');
     infoField.style.display = 'none'
+    let text = document.getElementById('text-field');
+    text.value = '';
+
+})
+document.getElementById('text-field').addEventListener('keyup',function(event){
+let text = event.target.value
+let btn = document.getElementById('delete-btn');
+if(text ==='delete'){
+    btn.removeAttribute('disabled')
+}
+else{
+    btn.setAttribute('disabled',true)
+}
+event.value = '';
 })
